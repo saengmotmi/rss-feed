1268e2a3c4bb67e51e51dd6d1e8b5c23f532661a
saengmotmi <ohjtack@gmail.com>
2022-03-13 00:25:58 +0900

    init

---
+++ src/styles/font.ts
@@ -0,0 +1,19 @@
+const font = `
+  @font-face {
+    font-family: 'Noto Sans CJK KR';
+    font-weight: normal;
+    src: url('/asset/fonts/NotoSansCJKkr-Regular.otf') format('opentype');
+  }
+  @font-face {
+    font-family: 'Noto Sans CJK KR';
+    font-weight: 700;
+    src: url('/asset/fonts/NotoSansCJKkr-Bold.otf') format('opentype');
+  }
+  @font-face {
+    font-family: 'Noto Sans CJK KR';
+    font-weight: 900;
+    src: url('/asset/fonts/NotoSansCJKkr-Black.otf') format('opentype');
+  }
+`;
+
+export default font;
