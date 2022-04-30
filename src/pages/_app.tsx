8c74f53c42da3b664b86da8b001383ca3e44df4b
saengmotmi <ohjtack@gmail.com>
2022-05-01 04:12:47 +0900

    add: mouse animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import dynamic from "next/dynamic";
+// @ts-ignore
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +15,16 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
+      <AnimatedCursor
+        // @ts-ignore
+        innerSize={15}
+        outerSize={8}
+        color="193, 11, 111"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+        clickables={["a"]}
+      />
     </>
   );
 }
