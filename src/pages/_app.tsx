562632f62bf61c0d464bd2b3c02cbab8d10000af
saengmotmi <ohjtack@gmail.com>
2022-07-31 16:11:56 +0900

    title 헤더 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         {/* width=device-width: 화면의 넓이를 디바이스의 넓이로 지정 */}
         {/* initial-scale=1: 초기 화면 배율 */}
+        <title>RSS Feed</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <style>{font}</style>
       </Head>
