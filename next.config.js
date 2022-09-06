533cc479b747c8e0d2cb1a8d98c97e14cd2715a3
saengmotmi <ohjtack@gmail.com>
2022-09-06 17:23:04 +0900

    delete: 주석처리 해둔 캐시 설정 제거

--- next.config.js
+++ next.config.js
@@ -12,38 +12,6 @@ const nextConfig = {
       "blogpfthumb-phinf.pstatic.net",
     ],
   },
-  // 이 설정들을 적용하면 ISR이 적용되지 않는다 -> 새로 빌드하는거 아니었나...?
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/rss",
-  //       headers: [
-  //         {
-  //           key: "Cache-Control",
-  //           value: "max-age=0, s-maxage=31536000",
-  //         },
-  //       ],
-  //     },
-  // {
-  //   source: "/_next/image",
-  //   headers: [
-  //     {
-  //       key: "Cache-Control",
-  //       value: "max-age=3600",
-  //     },
-  //   ],
-  // },
-  // {
-  //   source: "/assets",
-  //   headers: [
-  //     {
-  //       key: "Cache-Control",
-  //       value: "max-age=3600",
-  //     },
-  //   ],
-  // },
-  // ];
-  // },
 };
 
 module.exports = nextConfig;
