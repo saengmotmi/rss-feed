3ba554b579d062fe4048b61485d5ff6a723442f9
saengmotmi <ohjtack@gmail.com>
2022-10-11 01:26:40 +0900

    fix: error test

--- src/pages/rss/index.tsx
+++ src/pages/rss/index.tsx
@@ -19,6 +19,7 @@ export const getStaticProps = async () => {
     props: {
       dehydratedState: dehydrate(queryClient),
     },
-    revalidate: 60 * 60 * 3 /* ISR 3 hours */,
+    // revalidate: 60 * 60 * 3 /* ISR 3 hours */,
+    revalidate: 100 /* ISR 1 second */,
   };
 };
--- src/services/rss/feeds.ts
+++ src/services/rss/feeds.ts
@@ -6,8 +6,13 @@ export const getFeeds = async () => {
   const blogs = (isProduction ? BLOG_LIST : limitArray(BLOG_LIST, 3)).map(
     (blog) => blog.url
   );
+  let feeds;
+  try {
+    feeds = await Promise.all(blogs.map((url) => parser.parseURL(url)));
+    console.log({ feeds: JSON.stringify(feeds) });
+  } catch (error) {
+    console.log({ error });
+  }
 
-  const feeds = await Promise.all(blogs.map((url) => parser.parseURL(url)));
-
-  return formatFeeds(feeds);
+  return formatFeeds(feeds as any);
 };
