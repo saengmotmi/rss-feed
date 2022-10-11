93ed4598dca268de99ccf39661b7190944e1fefb
saengmotmi <ohjtack@gmail.com>
2022-10-12 03:23:19 +0900

    fix: 각 요청당 5초 제한

--- src/services/rss/feeds.ts
+++ src/services/rss/feeds.ts
@@ -1,13 +1,41 @@
 import { BLOG_LIST } from "components/pages/Rss/Rss.constant";
 import { formatFeeds, parser } from "components/pages/Rss/utils";
+import type { Feed } from "types/rss/rssApi";
+import { isFulfilled } from "types/utils";
 import { isProduction, limitArray } from "utils";
+
+const PAGE_LIMIT = 10;
 
 export const getFeeds = async () => {
   const blogs = (isProduction ? BLOG_LIST : limitArray(BLOG_LIST, 3)).map(
     (blog) => blog.url
   );
+  const result = await Promise.allSettled(
+    blogs.map((url) => getBlogWithTimeout(url, 3 * 1000))
+  );
 
-  const feeds = await Promise.all(blogs.map((url) => parser.parseURL(url)));
+  return limitArray(
+    formatFeeds(result.filter(isFulfilled).map((feed) => feed.value)),
+    3 * PAGE_LIMIT
+  );
+};
 
-  return limitArray(formatFeeds(feeds), 3 * 10);
+const getBlogWithTimeout = (url: string, delay: number) => {
+  return new Promise<Feed>((resolve, reject) => {
+    let data: Feed | undefined;
+
+    setTimeout(() => {
+      if (!data) {
+        console.log("reject", { url });
+        reject("timeout");
+      }
+    }, delay);
+
+    // TODO: head 요청으로 content-length만 먼저 떼올 수 있는 방법 있는지 체크
+    return parser.parseURL(url).then((res) => {
+      console.log("resolve", { url });
+      data = res;
+      resolve(res);
+    });
+  });
 };
--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,3 +1,11 @@
 import { Dispatch, SetStateAction } from "react";
 
 export type SetStateType<T> = Dispatch<SetStateAction<T>>;
+
+export const isRejected = (
+  input: PromiseSettledResult<unknown>
+): input is PromiseRejectedResult => input.status === "rejected";
+
+export const isFulfilled = <T>(
+  input: PromiseSettledResult<T>
+): input is PromiseFulfilledResult<T> => input.status === "fulfilled";
