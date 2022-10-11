62b049bb0410fb103ad9d6e962cfb43fc2259c2c
saengmotmi <ohjtack@gmail.com>
2022-10-12 05:19:19 +0900

    revalidate 복구

--- src/pages/rss/index.tsx
+++ src/pages/rss/index.tsx
@@ -19,7 +19,6 @@ export const getStaticProps = async () => {
     props: {
       dehydratedState: dehydrate(queryClient),
     },
-    // revalidate: 60 * 60 * 3 /* ISR 3 hours */,
-    revalidate: 60 /* ISR 60 second */,
+    revalidate: 60 * 60 * 3 /* ISR 3 hours */,
   };
 };
